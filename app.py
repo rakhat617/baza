43107b911e84e83cdbd70ec81a2db6ee56ba7483
rakhat617 <rakhat.as617@gmail.com>
2025-05-22 20:51:44 +0500

    dzCRUD

--- app.py
+++ app.py
@@ -10,32 +10,28 @@ conn = psycopg2.connect(
 cursor = conn.cursor()
 
 def create_table(table_name:str, fields:dict):
-    fields_info = ', '.join(f'{name} {type}' for name, type in fields.items())
+    fields_info = ', '.join(f'{name} {type}' for name, type in fields.items()) #GPT подсказала про метод join
     cursor.execute(f'''
     CREATE TABLE IF NOT EXISTS {table_name}(
         {fields_info}
         )
 ''')
 
-# create_table('fff', {'id':'SERIAL', 'name':'VARCHAR(60)', 'surname':'VARCHAR(100)'})
-
 def insert_data(table_name:str, data:list):
     for row in data:
         columns = ', '.join(row.keys())
-        values = "', '".join(str(value) for value in row.values())
+        placeholders = ', '.join(['%s'] * len(row)) #GPT подсказала как сделать нужное количество %s
+        values = list(row.values())
         cursor.execute(f'''
             INSERT INTO {table_name}({columns})
-            VALUES('{values}');
-        ''')
+            VALUES({placeholders});
+        ''', values)
         conn.commit()
 
-# insert_data('fff', [{'surname':'Abdrakhmanov', 'name':'Rakhat'}, {'name': 'Adilya'}])
-
-
-def select_data(table_name:str, fields='*', filter=None):
+def select_data(table_name:str, fields='*', filter=None): #Тут все сам придумал 
     if isinstance(fields, list):
         fields = ', '.join(fields)
-    if filter:
+    if filter: #Мб фильтер не очень, потому что условие юзер сам должен писать, но хз как еще
         filter = "WHERE " + filter
     cursor = conn.cursor()
     cursor.execute(f'''
@@ -45,9 +41,8 @@ def select_data(table_name:str, fields='*', filter=None):
     fetched_data = cursor.fetchall()
     return fetched_data
 
-
-def update_data(table_name:str, id:int, fields_new_values:dict):
-    for field in fields_new_values:
+def update_data(table_name:str, id:int, fields_new_values:dict): #Тут тоже все сам.
+    for field in fields_new_values: #Можно по идее запариться сделать чтоб несколько сразу рядов можно было обновить, но лень да и зачем
         cursor.execute(f'''
             UPDATE {table_name} 
             SET {field} = '{fields_new_values[field]}' 
@@ -55,33 +50,39 @@ def update_data(table_name:str, id:int, fields_new_values:dict):
         ''')
         conn.commit()
 
-def delete_data(table_name:str, id:int):
+def delete_data(table_name:str, id:int): #Тут тоже можно было сделать чтоб несколько рядов сразу, но зачем?
     cursor.execute(f"""
         DELETE FROM {table_name} WHERE id = {id}
     """)
     conn.commit()
 
-def drop_table(table_name:str):
+def drop_table(table_name:str): #Ну это изи
     cursor.execute(f"""
         DROP TABLE IF EXISTS {table_name}
     """)
     conn.commit()
 
-# a = select_data(table_name='fff')
-# print(a)
-# a = select_data(table_name='fff', fields=["name"], filter="name = 'Rakhat'")
-# print(a)
+create_table(table_name="customers", fields={
+    "id":"SERIAL PRIMARY KEY",
+    "first_name":"VARCHAR(50)",
+    "last_name":"VARCHAR(50)",
+    "age":"INT"})
 
-# update_data("fff", 1, {'name':'Ruslan'})
+insert_data(table_name="customers", data=[{"first_name":"Rakhat", "last_name":"Abdrakhmanov", "age":24},
+                                          {"first_name":"Ruslan", "age":23},
+                                          {"first_name":"Zhanel", "last_name":"Mektepbayeva"},
+                                          {"last_name":"Abishev", "age":21, "first_name":"Bekzhan"}])
 
-# delete_data("fff", 1)
+print(select_data(table_name="customers", fields=["first_name", "age"], filter="age is not NULL"))
 
-# a = select_data(table_name='fff')
-# print(a)
+update_data(table_name="customers", id=3, fields_new_values={"age":27})
+print(select_data(table_name="customers", fields=["first_name", "age"], filter="age is not NULL"))
 
-# drop_table("aaa")
+delete_data(table_name="customers", id=4)
+print(select_data(table_name="customers"))
 
-
+drop_table(table_name="customers")
+print(select_data(table_name="customers"))
 
 cursor.close()
 conn.close()
